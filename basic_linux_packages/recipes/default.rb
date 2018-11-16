c9477f4af105c751f77fd583b9ccb15cfbc7b54f
Sooraj M <sooraj@lifetrenz.com>
2018-11-16 18:08:00 +0530

    Changed apt_update syntax. Incremented Version

--- basic_linux_packages/recipes/default.rb
+++ basic_linux_packages/recipes/default.rb
@@ -3,5 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-apt_update
+apt_update 'Update the apt-cache regularly' do
+	frequency 84_300
+	action :periodic
+end
 package ['figlet', 'vim', 'htop', 'curl','wget']
